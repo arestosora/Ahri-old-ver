0aa42779495cc2ece04dcc151bbaf4013509f5e2
Rain <adonymestra2@gmail.com>
2023-08-01 23:06:22 -0500

    Embed Improvement

--- src/interaction-handlers/buttons/precios/combo.ts
+++ src/interaction-handlers/buttons/precios/combo.ts
@@ -18,24 +18,25 @@ export class ButtonHandler extends InteractionHandler {
   public async run(interaction: ButtonInteraction) {
 
     const embed = new EmbedBuilder()
+        .setTitle('<a:pinkwarn:1134704471637770300> Estos Combos están disponibles unicamente para Colombia :flag_co:')
         .setDescription('**Nuestros combos de Artesanía Hextech y Skins** <:price:1135058349621252106>')
-        .setColor(Colors.DarkVividPink)
+        .setColor(Colors.Aqua)
         .setFields([
             {
-                "name": "Combo 1", value: '<a:arrow:1135684369625923685> PASE SOUL FIGHTER + \`2\` Orbes + Cofre Hextech = **$41.000 COP**'
+                "name": "<:ChannelInfo:1134760898045681754> Combo 1", value: '**PASE SOUL FIGHTER**<:soulfightereventpass:1136127461562781817> \n \`2\` **Orbes** <:soulfighterorb:1136127437659455569>\n**Cofre Hextech** <:cofre:1136127499680624684>\n **$41.000 COP**', inline: true
             },
             {
-                "name": "Combo 2", value: '<a:arrow:1135684369625923685> SKIN \`1350\` RP + Cofre Artesano = **27.000 COP**'
+                "name": "<:ChannelInfo:1134760898045681754> Combo 2", value: '**SKIN** \`1350\` **RP** <a:aestheticheart:1135277052023554188>\n **Cofre Artesano** <:artesan2:1136127584921460757>\n**27.000 COP**', inline: true
             },
             {
-                "name": "Combo 3", value: '<a:arrow:1135684369625923685> SKIN \`1820\` RP + \`1\` Orbe + Cofre Artesano + Llave = **42.000 COP**'
+                "name": "<:ChannelInfo:1134760898045681754> Combo 3", value: '**SKIN** \`1820\` **RP** <a:aestheticheart:1135277052023554188>\n\`1\` **Orbe** <:soulfighterorb:1136127437659455569>\n **Cofre Artesano &Llave** <:artesan:1136127811078332578>\n **42.000 COP**', inline: true
             }
             ,
             {
-                "name": "Combo 4", value: '<a:arrow:1135684369625923685> \`11\` Cofres Maestro Artesano + Llaves = **41.000 COP**'
+                "name": "<:ChannelInfo:1134760898045681754> Combo 4", value: '\`11\` **Cofres Maestro Artesano & Llaves** <:artesan:1136127811078332578>\n **41.000 COP**', inline: true
             },
             {
-                "name": "Combo 5", value: '<a:arrow:1135684369625923685> \`5\` Cofres Maestro Artesano + Llaves \`(5)\` + \`1\` Orbe + Cofre Hextech = **27.000 COP**'
+                "name": "<:ChannelInfo:1134760898045681754> Combo 5", value: '\`5\` **Cofres Maestro Artesano & Llaves**<:artesan2:1136127584921460757> \n\`1\` **Orbe** <:soulfighterorb:1136127437659455569> & **Cofre Hextech** <:cofre:1136127499680624684>\n **27.000 COP**', inline: true
             },
             
         ])
